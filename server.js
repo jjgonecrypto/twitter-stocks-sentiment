764514fb323a2a43cf7b488aec73570bc160ba85
justin j. moses <justinjmoses@gmail.com>
2012-10-03 22:45:58 -0400

    Updating server

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var connect = require('connect');
+var port = 3001;
 
 connect()
   // disabled compress for development where data size isn't enough to make it worth
   // .use( connect.compress() )
-  .use( connect.logger() )
+  .use( connect.logger({format:"dev"}) )
   .use( connect.static( __dirname + '/') )
   // also experiencing worse performance in dev while using in-memory caching
   // .use( connect.staticCache() )
-  .listen( 3000 );
+  .listen( port );
+
+console.log("Connect server listening on port %d", port);
